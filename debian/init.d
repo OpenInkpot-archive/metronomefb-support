ad2eabfc8ca0edf096c85207aa9fdfccd52721e4
Yauhen Kharuzhy <jekhor@gmail.com>
2009-04-14 12:48:12 +0300

    Add metronometempd to initscripts
    Signed-off-by: Yauhen Kharuzhy <jekhor@gmail.com>

--- debian/init.d
+++ debian/init.d
@@ -1,8 +1,8 @@
 #! /bin/sh
 
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
-DESC="metronomefb support"
-NAME="metronomefb-support"
+DESC="metronome temperature daemon"
+NAME="metronometempd"
 
 [ -f /etc/default/metronomefb-support ] && . /etc/default/metronomefb-support
 
@@ -13,12 +13,21 @@ case "$1" in
 		temp_deg=`expr $temp / $TEMPERATURE_DIVIDER`
 		modprobe metronomefb temp=$temp_deg
 		modprobe $BOARD_MODULE
-		echo $DEFIO_DELAY > /sys/class/graphics/fb$FB_NUM/defio_delay 
+		echo $DEFIO_DELAY > /sys/class/graphics/fb$FB_NUM/defio_delay
 		echo "done."
+		echo -n "Starting metronome temperature daemon: "
+		start-stop-daemon --start --quiet --pidfile /var/run/metronometempd.pid --exec /usr/sbin/metronometempd -- $TEMPERATURE_SOURCE /sys/class/graphics/fb$FB_NUM/temp $TEMPERATURE_DIVIDER $PERIOD
+		echo "metronometempd."
 		;;
 	stop)
 		echo -n "Stopping $DESC: "
+		start-stop-daemon --stop --quiet --pidfile /var/run/metronometempd.pid --oknodo
 		echo "$NAME."
+		;;
+	restart)
+		$0 stop
+		sleep 1
+		$0 start
 		;;
 	*)
 		N=/etc/init.d/$NAME
